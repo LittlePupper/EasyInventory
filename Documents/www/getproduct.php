c2c49b9e798d32363c93f223009ed47edc9f0eb7
LITTLEPUPPER420\Binky 2.0 <noraw@telus.net>
2018-04-12 15:23:58 -0600

    PHP updates

--- Documents/www/getproduct.php
+++ Documents/www/getproduct.php
@@ -5,9 +5,9 @@
 	$result = mysqli_query($conn, $mysql_query);
 	if (mysqli_num_rows($result) > 0) {
 		while($row = mysqli_fetch_array($result)) {
-			echo $row['ProductName'];
+			echo $row['ProductID'] . '~' . $row['ProductName'] . '~' . $row['Price'] . '~' . $row['Size'] . '~' . $row['Unit'] . '~' . $row['Description'] . '~' . $row['Stock'];
 		}			
 	} else {
-		echo "Product search failure";
+		echo "No products found";
 	}
 ?>
